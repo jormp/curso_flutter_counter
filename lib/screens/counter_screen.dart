9803bc4063aa2ec0cee34f05b462152ee07f6e07
jorge.martinez <jorge.martinez@sandos.com>
2023-03-08 22:57:09 +0100

    Exercise ended

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -9,6 +9,21 @@ class CounterScreen extends StatefulWidget {
 
 class _CounterScreenState extends State<CounterScreen> {
   int counter = 0;
+  void increase() {
+    counter++;
+    setState(() {});
+  }
+
+  void decrease() {
+    counter--;
+    setState(() {});
+  }
+
+  void clear() {
+    counter = 0;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     const fontSize30 = TextStyle(fontSize: 30);
@@ -32,30 +47,36 @@ class _CounterScreenState extends State<CounterScreen> {
           ],
         ),
       ),
-      floatingActionButton:
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        FloatingActionButton(
-            child: const Icon(Icons.remove),
-            onPressed: () {
-              setState(() {
-                counter--;
-              });
-            }),
-        FloatingActionButton(
-            child: const Icon(Icons.deselect),
-            onPressed: () {
-              setState(() {
-                counter = 0;
-              });
-            }),
-        FloatingActionButton(
-            child: const Icon(Icons.add),
-            onPressed: () {
-              setState(() {
-                counter++;
-              });
-            }),
-      ]),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: CustomFloatingButton(
+        increaseFn: increase,
+        decreaseFn: decrease,
+        clearFn: clear,
+      ),
     );
+  }
+}
+
+class CustomFloatingButton extends StatelessWidget {
+  final Function increaseFn;
+  final Function decreaseFn;
+  final Function clearFn;
+  const CustomFloatingButton({
+    super.key,
+    required this.increaseFn,
+    required this.decreaseFn,
+    required this.clearFn,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+      FloatingActionButton(
+          onPressed: () => decreaseFn(), child: const Icon(Icons.remove)),
+      FloatingActionButton(
+          onPressed: () => clearFn(), child: const Icon(Icons.deselect)),
+      FloatingActionButton(
+          onPressed: () => increaseFn(), child: const Icon(Icons.add)),
+    ]);
   }
 }
